8a6fe7a1c7cccbb3cb96b3c9fb078a67fbad5eed
Matias <matias_gomez05@hotmail.com>
2024-11-04 21:18:17 -0300

    Config para desarrollo local

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,7 @@
-const endpoint = 'http://192.168.0.89:8000/api';
+
+//const endpoint = 'http://192.168.0.89:8000/api';
+const endpoint = 'http://192.168.1.40:8000/api';
+
 
 const endpoints = {
   productos: `${endpoint}/productos`,
